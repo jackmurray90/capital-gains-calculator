09340b5a87be2ea913b4779ac9582f7ad2db7b42
Jack Murray <jack@tradeapi.net>
2023-06-02 22:04:46 +1000

    add support for coinspot csvs and multiple binance csvs

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # Binance Capital Gains Tax Calculator
 
-Script that takes Binance trades CSV and calculates capital gains for a year.
-If anybody uses this I would appreciate an email to jack@murray.software so I
-can collect stats about usage. For new feature requests please create an issue.
+Script that takes Binance and CoinSpot trades CSV and calculates capital gains
+for each year.
 
 ## Usage
 
 Put your binance trades from the long-term (>6 month) export feature on binance
-into a CSV called "trades.csv", including headers. Then run:
+into a CSV file in the same directory. Do the same with CoinSpot. Then run:
 
     ./capital-gains-calculator.py
-
-To get more than 1 year worth of trades, stitch together multiple CSV exports.
-The order of trades doesn't matter, since they are provided with a timestamp.
 
 ## How it works
 
@@ -22,18 +18,14 @@ and matches up sells with buys in first-in-first-out order. It applies a 50%
 discount on capital gains held for over 1 year, and also provides the total
 without any discounts.
 
-To configure the script, look at the first 4 lines:
+To configure the script, look at the first 3 lines:
 
     australian_tax_year = True
     base = 'BTC'
     quote = 'AUD'
-    current_rate = 63189.59
 
 Setting `australian_tax_year` to False will mean that the tax year will be set
 to the normal Jan 1 -> Dec 31 instead of Jul 1 -> Jun 30 as they do in
 Australia.
 
 Setting base or quote will change the market.
-
-The `current_rate` is used to calculate your current theoretical profit at the
-end.
--- capital-gains-calculator.py
+++ capital-gains-calculator.py
@@ -3,50 +3,64 @@
 australian_tax_year = True
 base = 'BTC'
 quote = 'AUD'
-current_rate = 63189.59
 
-import csv
+from csv import DictReader
 from datetime import datetime
-import collections
+from collections import defaultdict
+from pathlib import Path
+
+coinspot_keys = {"Transaction Date", "Type", "Market", "Amount", "Rate inc. fee", "Rate ex. fee", "Fee", "Fee AUD (inc GST)", "GST AUD", "Total AUD", "Total (inc GST)"}
 
 rows = []
 
-with open('trades.csv', newline='') as csvfile:
-  reader = csv.DictReader(csvfile)
-  for row in reader:
-
-    # hack to help with efbbbf bytes at start of binance CSV export
-    for key, value in row.items():
-      if key.endswith('Date(UTC)'):
-        date = value
-    row['Date(UTC)'] = date
-
-    if row['Pair'] == base + quote:
-      timestamp = datetime.fromisoformat(row['Date(UTC)'])
-      btc = float(row['Executed'][:-3])
-      aud = float(row['Amount'][:-3].replace(",",""))
-      fee = float(row['Fee'][:-3].replace(",",""))
-      feecoin = row['Fee'][-3:]
-      if row['Side'] == 'BUY':
-        if feecoin == base:
-          btc -= fee
+for f in Path('.').iterdir():
+  if f.suffix == '.csv':
+    with f.open(newline='') as csvfile:
+      reader = DictReader(csvfile)
+      for row in reader:
+        # hack to help with efbbbf bytes at start of binance CSV export
+        date = None
+        for key, value in row.items():
+          if key.endswith('Date(UTC)'):
+            date = value
+        if date:
+          row['Date(UTC)'] = date
+        if row.keys() == coinspot_keys:
+          if row['Market'] == base + '/' + quote:
+            timestamp = datetime.strptime(row['Transaction Date'], "%d/%m/%Y %H:%M %p")
+            btc = float(row['Amount'])
+            aud = float(row['Total AUD'])
+            if row['Type'] == 'Buy':
+              rows.append((timestamp, 'buy', btc, aud))
+            else:
+              rows.append((timestamp, 'sell', btc, aud))
         else:
-          btc -= fee * (btc/aud)
-        rows.append((timestamp, 'buy', btc, aud))
-      else:
-        if feecoin == quote:
-          aud -= fee
-        else:
-          aud -= fee * (aud/btc)
-        rows.append((timestamp, 'sell', btc, aud))
+          if row['Pair'] == base + quote:
+            timestamp = datetime.fromisoformat(row['Date(UTC)'])
+            btc = float(row['Executed'][:-3])
+            aud = float(row['Amount'][:-3].replace(",",""))
+            fee = float(row['Fee'][:-3].replace(",",""))
+            feecoin = row['Fee'][-3:]
+            if row['Side'] == 'BUY':
+              if feecoin == base:
+                btc -= fee
+              else:
+                btc -= fee * (btc/aud)
+              rows.append((timestamp, 'buy', btc, aud))
+            else:
+              if feecoin == quote:
+                aud -= fee
+              else:
+                aud -= fee * (aud/btc)
+              rows.append((timestamp, 'sell', btc, aud))
 
 rows.sort()
 
 buys = []
 
 all_total_profit = 0
-total_profit = collections.defaultdict(lambda: 0)
-total_discounted_profit = collections.defaultdict(lambda: 0)
+total_profit = defaultdict(lambda: 0)
+total_discounted_profit = defaultdict(lambda: 0)
 
 for (timestamp, side, btc, aud) in rows:
   if side == 'buy':
@@ -74,6 +88,9 @@ for (timestamp, side, btc, aud) in rows:
         tax_year = timestamp.year-1 if timestamp.month <= 6 else timestamp.year
       else:
         tax_year = timestamp.year
+      if len(buys) == 0:
+        print(amount, "BTC produced out of thin air, claiming it was free at", rows[0][0])
+        buys = [{'timestamp': rows[0][0], 'btc': amount, 'aud': 0}]
       discount = buys[0]['timestamp'].replace(year=buys[0]['timestamp'].year+1) < timestamp
       print("Made", '$'+str(profit)[:9], "with", "%0.8f"%amount, "buying at", int(buying_rate),"on",buying_timestamp,"selling at", int(selling_rate), "on", timestamp, "50% discount" if discount else "no discount")
       all_total_profit += profit
@@ -86,10 +103,3 @@ print()
 for year in total_profit.keys():
   print("Total Gapital Gains for year starting","Jul" if australian_tax_year else "Jan",year,"is               ", total_profit[year])
   print("Total Gapital Gains for year starting","Jul" if australian_tax_year else "Jan",year,"with discounts is", total_discounted_profit[year])
-print()
-remaining_btc = sum([buy['btc'] for buy in buys])
-remaining_spent = sum([buy['aud'] for buy in buys])
-print("Total remaining btc is", remaining_btc, "( $", remaining_btc * current_rate, ", acquired for $", remaining_spent, ")")
-print()
-print("Total all-time profit if you sold everything now:", remaining_btc * current_rate - remaining_spent + all_total_profit)
-print()
